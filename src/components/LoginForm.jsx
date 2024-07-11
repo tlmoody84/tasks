609c70c32a06eada8c95f5f5c65838d0c86e9769
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-07-10 20:21:14 -0400

    vercel cann't resolve authutils

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { login } from "../utils/authutils";
+import { login } from "../src/app/utils/authutils";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
